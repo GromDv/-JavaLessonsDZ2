1ff4a137963263d70c4aaf9171c44e54da9a7f14
GromDv <gromdv@gmail.com>
2023-02-15 16:36:28 +0300

    Add Logger

--- Java_DZ2.java
+++ Java_DZ2.java
@@ -5,13 +5,22 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.logging.*;
 
 /**
  * Java_DZ2
  */
 public class Java_DZ2 {
 
-    public static void main(String[] args) {
+    static Logger logger = Logger.getLogger(Java_DZ2.class.getName());
+
+    public static void main(String[] args) throws SecurityException, IOException {
+
+        FileHandler fh = new FileHandler("log.txt");
+        logger.addHandler(fh);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+        logger.info("Начало!");
 
         int indata[] = readData("input.txt");
         double pow = power(indata[1], indata[0]);
@@ -19,10 +28,12 @@ public class Java_DZ2 {
         System.out.printf("Число %d в степени %d  = %.2f\n", indata[1], indata[0], pow);
 
         String d_out = String.format("%.2f", pow);
+        logger.info(String.format("Выходные данные: %s", d_out));
         writeData("output.txt", d_out);
     }
 
     static double power(int a, int b) {
+        logger.info(String.format("Расчёт при a = %d, b = %d", a, b));
         double res = 1;
         int p = b;
         if (b < 0)
@@ -46,6 +57,7 @@ public class Java_DZ2 {
                 res[i++] = Integer.valueOf(arr[1]);
             }
             bufReader.close();
+            logger.info("Входные данные прочитаны!");
         } catch (IOException ex) {
             System.out.println("Error reading!");
             System.out.println(ex.getMessage());
@@ -56,6 +68,7 @@ public class Java_DZ2 {
     static void writeData(String file, String data) {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
             bw.write(data);
+            logger.info("Выходные данные записаны!");
         } catch (IOException ex) {
             System.out.println("Error writing!");
             System.out.println(ex.getMessage());
